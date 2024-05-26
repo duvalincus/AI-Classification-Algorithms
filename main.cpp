c2222f09b6a214565ae61ac5876bf48e9b06ede1
Michael Eskola <101226715+mesko001@users.noreply.github.com>
2024-05-26 11:38:29 -0700

    Menu + Validation

--- main.cpp
+++ main.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
+#include <limits>
+
+using namespace std;
+
+int getValidNumber(int min, int max) {
+  int num = 0;
+  while (true) {
+    cin >> num;
+
+    if (cin.fail() || num < min || num > max) {
+      cout << "Invalid input, please enter a number between " << min << " and " << max << ".\n";
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    } else {
+      break;
+    }
+  }
+  return num;
+}
 
 int main() {
+  cout << "Welcome to CS170 Feature Selection Algorithm.\n";
+
+  cout << "Please enter total number of features:\n";
+  int number_of_features = getValidNumber(1, numeric_limits<int>::max());
+
+  cout << "Type the number of the algorithm you want to run.\n 1. Forward Selection\n 2. Backward Elimination\n 3. Bertie’s Special Algorithm.\n";
+  int algo_num = getValidNumber(1, 3);
+
+cout <<"Number of Features: " <<number_of_features <<" Type of Algorithm: " <<algo_num <<endl;
   
+  return 0;
 }
